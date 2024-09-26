823f6f2ffd0a6954174ab69aef98798bbd8506c8
AbdallahRustom <abdallah.rustom.ar@gmail.com>
2024-09-26 21:15:23 +0000

    init commit

--- Dockerfile
+++ Dockerfile
@@ -1,46 +1,48 @@
-FROM sonatype/nexus3
+# Use an official Ubuntu base image
+FROM ubuntu:20.04
 
-LABEL maintainer="Brad Beck <bradley.beck+docker@gmail.com>"
+# Set environment variables
+ENV NEXUS_VERSION=3.30.0-01
+ENV NEXUS_DOWNLOAD_URL=https://download.sonatype.com/nexus/3/nexus-${NEXUS_VERSION}-unix.tar.gz
+ENV SONATYPE_DIR=/opt/sonatype
+ENV NEXUS_HOME=${SONATYPE_DIR}/nexus
+ENV NEXUS_DATA=/nexus-data
+ENV INSTALL4J_ADD_VM_PARAMS="-Xms2703m -Xmx2703m -XX:MaxDirectMemorySize=2703m"
+ENV INSTALL4J_JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
-ENV NEXUS_SSL=${NEXUS_HOME}/etc/ssl
-ENV PUBLIC_CERT=${NEXUS_SSL}/cacert.pem \
-    PUBLIC_CERT_SUBJ=/CN=localhost \
-    PRIVATE_KEY=${NEXUS_SSL}/cakey.pem \
-    PRIVATE_KEY_PASSWORD=password
+# Install required packages
+RUN apt-get update && apt-get install -y \
+#    openjdk-11-jdk \ 
+    openjdk-8-jdk \
+    openssl \ 
+    libxml2 \
+    gosu \
+    curl \
+    && apt-get clean
 
-ARG GOSU_VERSION=1.13
+# Create necessary directories
+RUN mkdir -p ${NEXUS_HOME} ${NEXUS_DATA} && \
+    groupadd --gid 200 nexus && \
+    useradd --uid 200 --gid nexus --shell /bin/false --create-home nexus
 
-USER root
+# Download and install Nexus
+RUN curl -L ${NEXUS_DOWNLOAD_URL} -o nexus.tar.gz && \
+    tar -xzf nexus.tar.gz -C ${SONATYPE_DIR} && \
+    mv ${SONATYPE_DIR}/nexus-${NEXUS_VERSION} ${NEXUS_HOME} && \
+    rm nexus.tar.gz && \
+    chown -R nexus:nexus ${SONATYPE_DIR}
+RUN echo 17
 
-RUN sed -e '/^enabled=1/ s/=1/=0/' -i /etc/yum/pluginconf.d/subscription-manager.conf \
- && yum -y update && yum install -y openssl libxml2 libxslt && yum clean all
 
-RUN set -eux;\
-    curl -o /usr/local/bin/gosu -SL "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-amd64"; \
-    curl -o /usr/local/bin/gosu.asc -SL "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-amd64.asc"; \
-    gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4; \
-    gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu; \
-    rm -rf /root/.gnupg/ /usr/local/bin/gosu.asc; \
-    command -v gpgconf && gpgconf --kill all || :; \
-    chmod +x /usr/local/bin/gosu; \
-    gosu --version; \
-    gosu nobody true
-
-RUN sed \
-    -e '/^nexus-args/ s:$:,${jetty.etc}/jetty-https.xml:' \
-    -e '/^application-port/a \
-application-port-ssl=8443\
-' \
-    -i ${NEXUS_HOME}/etc/nexus-default.properties
-
-COPY entrypoint.sh ${NEXUS_HOME}/entrypoint.sh
-RUN chown nexus:nexus ${NEXUS_HOME}/entrypoint.sh && chmod a+x ${NEXUS_HOME}/entrypoint.sh
-
-VOLUME [ "${NEXUS_SSL}" ]
-
+COPY ./entrypoint.sh /opt/sonatype/nexus/
+# Expose the necessary ports
+EXPOSE 8081
 EXPOSE 8443
+# Switch to the nexus user
+USER nexus
+
+# Set the working directory
 WORKDIR ${NEXUS_HOME}
 
-ENTRYPOINT [ "./entrypoint.sh" ]
-
-CMD [ "bin/nexus", "run" ]
+# Command to run Nexus
+CMD ["bash", "entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,47 @@
 #!/usr/bin/env bash
 
+cp -r /mnt/ssl/ /opt/sonatype/nexus/nexus-3.30.0-01/etc/
+cp /mnt/nexus-default.properties /opt/sonatype/nexus/nexus-3.30.0-01/etc/nexus-default.properties
+cd /opt/sonatype/nexus/nexus-3.30.0-01/etc/ssl
 set -x
 set -eo pipefail
 
-if [ "$1" == 'bin/nexus' ]; then
-  if [ ! -f "$NEXUS_SSL/keystore.jks" ]; then
-    mkdir -p $NEXUS_SSL
-    if [ ! -f $PUBLIC_CERT ] && [ ! -f $PRIVATE_KEY ]; then
-      openssl req -nodes -new -x509 -keyout $PRIVATE_KEY -out $PUBLIC_CERT -subj "${PUBLIC_CERT_SUBJ}"
-    fi
-    if [ ! -f $NEXUS_SSL/jetty.key ]; then
-      openssl pkcs12 -export -in $PUBLIC_CERT -inkey $PRIVATE_KEY -out $NEXUS_SSL/jetty.key -passout pass:$PRIVATE_KEY_PASSWORD
-    fi
-    $JAVA_HOME/bin/keytool -importkeystore -noprompt -deststorepass $PRIVATE_KEY_PASSWORD -destkeypass $PRIVATE_KEY_PASSWORD -destkeystore $NEXUS_SSL/keystore.jks -srckeystore $NEXUS_SSL/jetty.key -srcstoretype PKCS12 -srcstorepass $PRIVATE_KEY_PASSWORD
-    sed -r '/<Set name="(KeyStore|KeyManager|TrustStore)Password">/ s:>.*$:>'$PRIVATE_KEY_PASSWORD'</Set>:' -i $NEXUS_HOME/etc/jetty/jetty-https.xml
+if [ ! -f "$NEXUS_SSL/keystore.jks" ]; then
+
+  if [ ! -f $NEXUS_SSL/$PUBLIC_CERT ] && [ ! -f $NEXUS_SSL/$PRIVATE_KEY ]; then
+    openssl req -nodes -new -x509 -keyout $PRIVATE_KEY -out $PUBLIC_CERT -subj "${PUBLIC_CERT_SUBJ}"
   fi
-
-  mkdir -p "$NEXUS_DATA"
-  chown -R nexus:nexus "$NEXUS_DATA"
-
-  exec gosu nexus "$@"
+  if [ ! -f $NEXUS_SSL/jetty.key ]; then
+    openssl pkcs12 -export -in $PUBLIC_CERT -inkey $PRIVATE_KEY -out $NEXUS_SSL/keystore.p12 -name jetty -passout pass:$PRIVATE_KEY_PASSWORD
+  fi   
+  $JAVA_HOME/bin/keytool -importkeystore -noprompt -deststorepass $PRIVATE_KEY_PASSWORD -destkeypass $PRIVATE_KEY_PASSWORD -destkeystore $NEXUS_SSL/keystore.jks -srckeystore $NEXUS_SSL/keystore.p12 -srcstoretype PKCS12 -alias jetty -srcstorepass $PRIVATE_KEY_PASSWORD
+  sed -r '/<Set name="(KeyStore|KeyManager|TrustStore)Password">/ s:>.*$:>'$PRIVATE_KEY_PASSWORD'</Set>:' -i $NEXUS_HOME/etc/jetty/jetty-https.xml
 fi
+mkdir -p "$NEXUS_DATA"
+chown -R nexus:nexus "$NEXUS_DATA"
+
+JETTY_HTTPS_FILE="/opt/sonatype/nexus/nexus-3.30.0-01/etc/jetty/jetty-https.xml"
+TEMP_FILE=$(mktemp)
+
+while IFS= read -r line; do
+  if [[ "$line" == *"<Set name=\"KeyStorePath\">"* ]]; then
+    INDENTATION=$(echo "$line" | sed 's/[^ ]//g')
+    echo "${INDENTATION}<Set name=\"KeyStorePath\">/opt/sonatype/nexus/nexus-3.30.0-01/etc/ssl/keystore.jks</Set>" >> "$TEMP_FILE"
+    echo "${INDENTATION}<Set name=\"certAlias\">jetty</Set>" >> "$TEMP_FILE"
+  elif [[ "$line" == *"<Set name=\"TrustStorePath\">"* ]]; then
+    INDENTATION=$(echo "$line" | sed 's/[^ ]//g')
+    echo "${INDENTATION}<Set name=\"TrustStorePath\">/opt/sonatype/nexus/nexus-3.30.0-01/etc/ssl/keystore.jks</Set>" >> "$TEMP_FILE"
+  else
+    echo "$line" >> "$TEMP_FILE"
+  fi
+done < "$JETTY_HTTPS_FILE"
+
+mv "$TEMP_FILE" "$JETTY_HTTPS_FILE"
+
+echo "certAlias line added successfully."
+
+
+
+exec /opt/sonatype/nexus/nexus-3.30.0-01/bin/nexus run
 
 exec "$@"
